c64f2e99b1dfe73600a61aa0109b0203f3ae3dba
Nuez <hola.nuez.nqn@gmail.com>
2025-05-21 19:45:10 -0300

    Proyecto Nuez limpio y funcionando

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen bg-amber-50 flex items-center justify-center">
+      <h1 className="text-4xl text-gray-800 font-semibold">
+        Esto es Nuez 🍫
+      </h1>
+    </div>
+  );
+}
+
+export default App;
